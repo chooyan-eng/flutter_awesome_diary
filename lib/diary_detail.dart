7166c25c77f3c8dc951f2ab00b70f6f3f3ede8de
tsuyoshi-chujo <chooyan.engineer@gmail.com>
2020-05-13 00:41:50 +0900

    4-4まで

--- lib/diary_detail.dart
+++ lib/diary_detail.dart
@@ -1,4 +1,4 @@
-import 'package:awesome_diary/diary.dart';
+import 'package:awesome_diary/diary_provider.dart';
 import 'package:flutter/material.dart';
 
 class DiaryDetail extends StatelessWidget {
