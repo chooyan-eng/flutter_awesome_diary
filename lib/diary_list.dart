cc7818665c93752e42eb9f603736a90798085225
tsuyoshi-chujo <chooyan.engineer@gmail.com>
2020-05-13 01:22:31 +0900

    7-3まで

--- lib/diary_list.dart
+++ lib/diary_list.dart
@@ -3,6 +3,7 @@ import 'package:awesome_diary/diary_provider.dart';
 import 'package:awesome_diary/diary_detail.dart';
 import 'package:awesome_diary/diary_list_item.dart';
 import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
 
 class DiaryList extends StatefulWidget {
   @override
@@ -14,16 +15,34 @@ class _DiaryListState extends State<DiaryList> {
 
   @override
   void initState() {
-    final provider = DiaryProvider();
-    
-    provider.open().then((_) async {
-      final savedDiaryList = await provider.getAll();
-      setState(() {
-        _diaryList.addAll(savedDiaryList);
-      });
-    });
-
+    _updateList();
     super.initState();
+  }
+
+  Future<void> _updateList() async {
+    _diaryList.clear();
+    
+    final provider = DiaryProvider();
+    await provider.open();
+    final savedDiaryList = await provider.getAll();
+    await provider.close();
+    setState(() {
+      _diaryList.addAll(savedDiaryList);
+    });
+  }
+
+  Future<void> _delete(Diary diary) async {
+    final provider = DiaryProvider();
+    await provider.open();
+    final deletedItemCount = await provider.delete(diary.id);
+    await provider.close();
+
+    if (deletedItemCount > 0) {
+      Toast.show('日記を削除しました。', context);
+      await _updateList();
+    } else {
+      Toast.show('日記を削除できませんでした。', context);
+    }
   }
 
   @override
@@ -37,9 +56,7 @@ class _DiaryListState extends State<DiaryList> {
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(
-                    builder: (context) => CreateDiary()
-                  ),
+                  MaterialPageRoute(builder: (context) => CreateDiary()),
                 );
               },
               child: Icon(Icons.add),
@@ -50,6 +67,45 @@ class _DiaryListState extends State<DiaryList> {
       body: ListView.builder(
         itemCount: _diaryList.length,
         itemBuilder: (context, index) => InkWell(
+          onLongPress: () {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text('日記のメニュー'),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    InkWell(
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: double.infinity,
+                          child: Text(
+                            '変更',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () => _delete(_diaryList[index]),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: double.infinity,
+                          child: Text(
+                            '削除',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
           onTap: () {
             Navigator.push(
               context,
