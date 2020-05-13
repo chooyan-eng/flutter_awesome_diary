072bd5a6c1b2673fc58bffe446818d6aa38b3f7e
tsuyoshi-chujo <chooyan.engineer@gmail.com>
2020-05-13 17:04:48 +0900

    レイアウト調整

--- lib/diary_list_item.dart
+++ lib/diary_list_item.dart
@@ -21,14 +21,13 @@ class DiaryListItem extends StatelessWidget {
         child: InkWell(
           onLongPress: onLongPress,
           onTap: onTap,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                diary.imageFile != null ? 
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              diary.imageFile != null ? 
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(4.0),
+                  child: Container(
                     height: 80,
                     width: double.infinity,
                     child: Hero(
@@ -38,30 +37,40 @@ class DiaryListItem extends StatelessWidget {
                         fit: BoxFit.fitWidth,
                       ),
                     ),
-                  )
-                : SizedBox.shrink(),
-                Text(
-                  diary.title,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28,
                   ),
+                )
+              : SizedBox.shrink(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(height: 16),
+                    Text(
+                      diary.title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 28,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      diary.body,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                    SizedBox(height: 8),
+                    Align(
+                      child: Text(
+                        '${diary.createdAt.year}/${diary.createdAt.month}/${diary.createdAt.day}',
+                      ),
+                      alignment: Alignment.centerRight,
+                    ),
+                  ],
                 ),
-                SizedBox(height: 8),
-                Text(
-                  diary.body,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
-                ),
-                SizedBox(height: 8),
-                Align(
-                  child: Text(
-                    '${diary.createdAt.year}/${diary.createdAt.month}/${diary.createdAt.day}',
-                  ),
-                  alignment: Alignment.centerRight,
-                ),
-              ],
-            ),
+              ),
+              SizedBox(height: 16),
+            ],
           ),
         ),
       ),
